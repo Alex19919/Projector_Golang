78963d298517493cfc05a8126a14f95d073b6e9d
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2022-10-15 15:34:17 +0300

    NEwOne

--- HW1.2/main.go
+++ HW1.2/main.go
@@ -1,74 +1,40 @@
 // 2. Імітація лікаря. У вигляді опитування. Кожна відповідь веде до наступного запитання в терміналі
 
-
-// package main
-
-// import 	"fmt"
-
-
-// type Car struct {
-// 	Brand string
-// 	Model string
-// 	Year int
-// 	Engine float64
-// 	color string
-// }
-
-// var myCar = Car{"Ford", "Escape", 2015, 2.5, "Grey"}
-// fmt.Println(myCar)
-
-
-
 package main
 
-import (
-	"flag"
-	"fmt"
+import 	(
+  "fmt"
 )
 
-type region string
-type method string
+  func main() {
 
-const (
-	Europe  region = "e"
-	Asia    region = "s"
-	America region = "a"
+fmt.Println("Hello?: ")
 
-	Plane method = `p`
-	Train method = `t`
-	Bus   method = `b`
-)
+var greeting string
+fmt.Scanln(&greeting)  //hi
 
-func main() {
-	reg := flag.String("region", ``, "Region")
-	meth := flag.String("method", ``, "Method")
-	help := flag.Bool("help", false, "Help")
-	flag.Parse()
+fmt.Println("What is your name?: ")
 
-	if *help {
-		fmt.Print("Run program with region flag:\n" +
-			"main.go -region [e|s|a] \n" +
-			"and optional -method [p|t|b]\n" +
-			"For example: go run branchflags/main.go --region a --method t")
-	} else {
-		switch *reg {
-		case string(Europe):
-			fmt.Printf("Let's go to Europe!\n")
-		case string(Asia):
-			fmt.Printf("Let's go to Asia!\n")
-		case string(America):
-			fmt.Printf("Let's go to America!\n")
-		default:
-			fmt.Printf("No no no. Which region?\n")
-		}
-		switch *meth {
-		case string(Bus):
-			fmt.Printf("Bus is default method!\n")
-		case string(Train):
-			fmt.Printf("Train? Ok!\n")
-		case string(Plane):
-			fmt.Printf("Plane? Ok!\n")
-		default:
-			fmt.Printf("Bus is default method!\n")
-		}
-	}
+var name string
+fmt.Scanln(&name) //Alex
+
+fmt.Println("What is your age?: ")
+
+var age int
+fmt.Scanln(&age) //31
+
+fmt.Println("What is your weight?: ")
+
+var weight float64
+fmt.Scanln(&weight)  //100
+
+fmt.Println("What health problems do you have?: ")
+
+var problems string
+fmt.Scanln(&problems)  //headache
+
+fmt.Println("I will write you a prescription for medicine: ")
+
+var prescriptionForMedicine string
+fmt.Scanln(&prescriptionForMedicine)   //ok
+}
