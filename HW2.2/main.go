2fa0512c6a728cd3e9db9794f59478551648c934
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2022-10-20 17:07:22 +0300

    HW2

---
+++ HW2.2/main.go
@@ -0,0 +1,18 @@
+
+// • Робота із рядками. Виконання популярних
+// пошуків і конкатенація (всі функції пакета strings)
+
+package main
+
+import (
+	`fmt`
+)
+
+type _string struct {
+	elements *byte // underlying bytes
+	len      int   // number of bytes
+}
+
+func main() {
+	
+}
---
+++ HW2.3/main.go
@@ -0,0 +1,62 @@
+// • Калькулярот (працює із операторами - + / *)
+
+package main
+
+import 	(
+	"fmt"
+	"flag"
+)
+func main(){
+
+
+	 /////  мої взірці з пайтону 
+
+
+// a = int(input('First number: '))
+// b = int(input('Second number: '))
+
+// d = int(input('Choose an arithmetic operation: \n 1 addition \n 2 subtraction \n 3 multiplication \n 4 division \n'))
+
+// if d == 1:
+//     r = a+b
+//     p = 'addition'
+//     o = p
+// if d == 2:
+//     r = a-b
+//     l = 'subtraction'
+//     o = l
+// if d == 3:
+//     r = a*b
+//     m = 'multiplication'
+//     o = m
+// if d == 4:
+//     r = float(a/b)
+//     n = 'division'
+//     o = n
+// print('Result', o, 'number', a, 'and', b, '=', r)
+
+
+
+// import sys
+// # !=	Проверяет равны ли оба операнда. Если нет, то условие становится истинным.
+// if len(sys.argv) != 4:
+//     # raise  --- Возбуждает указанное исключение. Инструкция позволяет прервать штатный поток исполнения при помощи возбуждения исключения.
+//     raise Exception("Необхідна підтримка 4 базових арифметичних операцій")
+//     #sys.argv - список аргументов командной строки, передаваемых сценарию Python. sys.argv[0] является именем скрипта(пустой строкой в интерактивной оболочке).
+// if not type(sys.argv[1]) and type(sys.argv[3]) is int:
+//     raise TypeError("Помилка")
+
+// s = sys.argv[2]
+// if s in ('+', '-', '*', '/'):
+//     x = int(sys.argv[1])
+//     y = int(sys.argv[3])
+//     if s == '+':
+//         print(x+y)
+//     if s == '-':
+//         print(x-y)
+//     if s == '*':
+//         print(x*y)
+//     if s == '/':
+//         print(x/y)
+
+}
